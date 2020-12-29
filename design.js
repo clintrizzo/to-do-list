cb767193b8c31578eb4fa5a215c3d40b32ed4430
Clint <maxeva02@gmail.com>
2020-12-28 19:07:07 -0800

    set up the local storage for the todos

--- design.js
+++ design.js
@@ -7,10 +7,31 @@ var todos = [];
 
 intit();
 
-function renderTodos();
-todoList.innerHTML = "";
-todoCountSpan = todos.length;
+function renderTodos() {
+    todoList.innerHTML = "";
+    todoCountSpan = todos.length;
 
-for (var i = 0; i < todos.length; i++) {
-    var todo = todos[i];
+    for (var i = 0; i < todos.length; i++) {
+        var todo = todos[i];
+
+        var li = document.createElement("li");
+        li.textContent = todo;
+        li.setAttribute("data-index", i);
+
+        var button = document.createElement("button");
+        button.textContent = "Complete";
+
+        li.appendChild(button);
+        todoList.appendChild(li);
+    }
+}
+
+function intit() {
+    var storedTodos = JSON.parse(localStorage.getItem("todos"));
+
+    if (storedTodos !== null) {
+        todos = storedTodos;
+    }
+
+    renderTodos();
 }
